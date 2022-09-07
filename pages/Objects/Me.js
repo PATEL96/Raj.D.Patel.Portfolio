9f9828290fe4e84cd6b4956a39dd5c239678a444
PATEL96 <patel.raj.official.96@gmail.com>
2022-09-08 00:53:42 +0530

    feat: Home.Me(v.02);

--- pages/Objects/Me.js
+++ pages/Objects/Me.js
@@ -1,16 +1,42 @@
 import styles from '../../styles/Me.module.css';
 import profile from '../Props/Profile2.jpeg'
 import Image from 'next/image';
+import styled from 'styled-components'
+import { useSpring, animated } from 'react-spring';
+
+const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1]
+const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
 
 export default function Me() {
+
+    const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 10, tension: 250, friction: 100 } }));
+
     return (
-        <div className={styles.Container}>
+        <Container onMouseMove={({ clientX: x, clientY: y }) => (set({ xys: calc(x, y) }))} onMouseLeave={() => set({ xys: [0, 0, 1] })} style={{ transform: props.xys.interpolate(trans) }} >
             <Image src={profile} height={450} width={350} className={styles.Image} />
             <div className={styles.dataArea}>
-                <div className={styles.Data}>
+                <div className={styles.Data1}>
                     Raj D. Patel
                 </div>
+                <div className={styles.Data2}>
+                    Hi I'm Raj! I'm Really Fond of Learning New stuff
+                </div>
             </div>
-        </div>
+        </Container>
     );
 }
+
+const Container = styled(animated.div)`
+    display: flex;
+    justify-content: space-around;
+    padding: 3rem;
+    background: #c7d2fe2e;
+    border-radius: 10px;
+    z-index: 1;
+    position: relative;
+    backdrop-filter: blur(10px);
+    margin-right: 100px;
+    margin-left: 100px;
+    background-clip: border-box;
+    cursor: pointer;
+`
